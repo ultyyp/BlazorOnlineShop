5280ff5b961b2e6bdfc73f0988f8fc8c26e8b6bb
ultyyp <yehia.super@gmail.com>
2023-06-30 18:38:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using BlazorOnlineShop.Interfaces;
+using BlazorOnlineShop.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton<IClock, CurrentClock>();
+builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();
+//builder.Services.AddSingleton<NavigationManager>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
